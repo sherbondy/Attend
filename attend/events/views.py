03c111026e86d5e35657c7672f244a6e3f1e05c4
Ethan Sherbondy <sherbondye@gmail.com>
2011-01-31 02:01:57 -0500

    Adding socialregistration.

--- attend/events/views.py
+++ attend/events/views.py
@@ -5,10 +5,13 @@ from django.template import RequestContext
 from django.contrib.auth.models import User
 from django.contrib.auth import logout
 
+import facebook
+
 def home(request):
     c = RequestContext(request)
     
     if request.user.is_authenticated and request.user.is_active:
+        profile = facebook.GraphAPI(fb_user['access_token']).get_object('me')
         return render_to_response('events.html', {}, context_instance=c)
     else:
         return render_to_response('index.html', {}, context_instance=c)
