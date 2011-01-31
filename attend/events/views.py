d1d8653db20fc6f4515e2f7d7dcb1d32c9052edd
Ethan Sherbondy <sherbondye@gmail.com>
2011-01-30 22:32:44 -0500

    Initial commit.

---
+++ attend/events/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import get_object_or_404, render_to_response
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+from django.template import RequestContext
+
+def home(request):
+    return render_to_response('index.html')
