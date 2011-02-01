499ca6bb8d53b3882733232de794a82dbc2ee90a
Ethan Sherbondy <sherbondye@gmail.com>
2011-01-31 21:42:38 -0500

    Workaround for the inexplicable facebook object has no attribute graph bug.

--- attend/events/views.py
+++ attend/events/views.py
@@ -31,6 +31,14 @@ END:VEVENT
 END:VCALENDAR
 """
 
+# convenience method to avoid strange AttributeError
+# complaining the facebook object has no attribute graph
+def fb(request):
+    try:
+        return request.facebook.graph
+    except AttributeError:
+        return False
+    
 def logged_in(request):
     return request.user.is_authenticated and request.user.is_active
     
@@ -43,15 +51,15 @@ def start_end(event):
 # custom render function, vars is a dictionary, request is the request object
 def render(template, vars, request):
     c = RequestContext(request)
-    if logged_in(request):
-        vars["me"] = request.facebook.graph.get_object("me")
+    if fb(request) and logged_in(request):
+        vars["me"] = fb(request).get_object("me")
     return render_to_response(template, vars, context_instance=c)
         
 
 def home(request):    
-    if logged_in(request):
-        fbuser = request.facebook.graph.get_object("me")
-        events = request.facebook.graph.get_connections("me", "events")
+    if fb(request) and logged_in(request):
+        fbuser = fb(request).get_object("me")
+        events = fb(request).get_connections("me", "events")
         real_events = []
         now = datetime.now()
 
@@ -59,20 +67,20 @@ def home(request):
         db_event_ids = []
         for db_event in FacebookEvent.objects.filter(facebook_user_id=fbuser["id"]):
             db_event_ids.append(db_event.facebook_event_id)
-            new_db_event = request.facebook.graph.get_object(db_event.facebook_event_id)
+            new_db_event = fb(request).get_object(db_event.facebook_event_id)
             new_db_event = start_end(new_db_event)
             db_events.append(new_db_event)
 
         for event in events["data"]:
             event = start_end(event)
-    
+
             if event["rsvp_status"] == u'attending' and event["start_dt"] > now:
-                new_event = request.facebook.graph.get_object(event["id"])
+                new_event = fb(request).get_object(event["id"])
                 new_event = start_end(new_event)
-        
+    
                 if new_event["owner"]["id"] == fbuser["id"] and new_event["privacy"] == u'OPEN' and not new_event["id"] in db_event_ids: 
                     real_events.append(new_event)
-    
+
         return render('events.html', {'me':fbuser, 'events':real_events, 'db_events':db_events}, request)
     else:
         return render('index.html', {}, request)
@@ -93,15 +101,15 @@ def event(request,event_id):
     try:
         event_obj = FacebookEvent.objects.get(facebook_event_id=event_id)
     except FacebookEvent.DoesNotExist:
-        if logged_in(request):
-            fbuser = request.facebook.graph.get_object("me")
-            fbevent = request.facebook.graph.get_object(event_id)
+        if fb(request) and logged_in(request):
+            fbuser = fb(request).get_object("me")
+            fbevent = fb(request).get_object(event_id)
             if fbevent["owner"]["id"]==fbuser["id"]: #create a QR code
                 event_obj = FacebookEvent(facebook_event_id=event_id,facebook_user_id=fbuser["id"])
                 event_obj.save()
     
     if event_obj:
-        event = request.facebook.graph.get_object(event_id)
+        event = fb(request).get_object(event_id)
         event = start_end(event)
         return render('event.html', {'event_id':event_id,'event_obj':event}, request)
     else:
