fc733c16f5b5414e2cf258ef3b9c38afcbd8d758
Ethan Sherbondy <sherbondye@gmail.com>
2011-11-23 02:51:05 -0500

    bah, now I have the correct templates.

--- attend/events/views.py
+++ attend/events/views.py
@@ -1,3 +1,4 @@
+from urllib import quote
 from django.shortcuts import get_object_or_404, render_to_response, redirect
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
@@ -5,59 +6,84 @@ from django.template import RequestContext
 from django.contrib.auth.models import User
 from django.contrib.auth import logout
 from django.core import mail
+from django.core.validators import validate_email
+from django import forms
 from datetime import datetime
 from events.models import *
 import facebook
-import uuid
 
-def home(request):
-    c = RequestContext(request)
+ical = \
+"""BEGIN:VCALENDAR
+VERSION:2.0
+BEGIN:VEVENT
+DTSTART:{0}
+DTEND:{1}
+SUMMARY:{2}
+LOCATION:{3}
+DESCRIPTION:{4}
+URL;VALUE=URI:http://www.facebook.com/event.php?eid={5}
+BEGIN:VALARM
+TRIGGER:-PT15M
+DESCRIPTION:QRAttend event reminder
+ACTION:DISPLAY
+END:VALARM
+END:VEVENT
+END:VCALENDAR
+"""
+
+# convenience method to avoid strange AttributeError
+# complaining the facebook object has no attribute graph
+def fb(request):
+    try:
+        return request.facebook.graph
+    except AttributeError:
+        return False
     
-    if request.user.is_authenticated and request.user.is_active:
-        fbuser = request.facebook.graph.get_object("me")
-        events = request.facebook.graph.get_connections("me", "events")
+def logged_in(request):
+    return request.user.is_authenticated and request.user.is_active
+    
+# returns event object with added start_dt and end_dt parameters for convenience
+def start_end(event):
+    event["start_dt"] = datetime.strptime(event["start_time"], "%Y-%m-%dT%H:%M:%S")
+    event["end_dt"] = datetime.strptime(event["end_time"], "%Y-%m-%dT%H:%M:%S")
+    return event
+
+# custom render function, vars is a dictionary, request is the request object
+def render(template, vars, request):
+    c = RequestContext(request)
+    if fb(request) and logged_in(request):
+        vars["me"] = fb(request).get_object("me")
+    return render_to_response(template, vars, context_instance=c)
+        
+
+def home(request):    
+    if fb(request) and logged_in(request):
+        fbuser = fb(request).get_object("me")
+        events = fb(request).get_connections("me", "events")
         real_events = []
         now = datetime.now()
-	
-	friends = request.facebook.graph.get_object("me/friends")
-	friend_hash = {}
-	for friend in friends["data"]:
-            friend_hash[friend["id"]]=friend["name"]
-	friend_events = []
-	for db_event in FacebookEvent.objects.all():
-	    if db_event.facebook_user_id in friend_hash:
-                friend_event = request.facebook.graph.get_object(db_event.facebook_event_id)
-                friend_event["start_dt"] = datetime.strptime(friend_event["start_time"], "%Y-%m-%dT%H:%M:%S")
-                friend_event["end_dt"] = datetime.strptime(friend_event["end_time"], "%Y-%m-%dT%H:%M:%S")
-                friend_event["friend_name"] = friend_hash[db_event.facebook_user_id]
-                friend_event["friend_id"] = db_event.facebook_user_id
-                friend_events.append(friend_event)
-	
-	db_events = []
-	db_event_ids = []
-	for db_event in FacebookEvent.objects.filter(facebook_user_id=fbuser["id"]):
-	    db_event_ids.append(db_event.facebook_event_id)
-            new_db_event = request.facebook.graph.get_object(db_event.facebook_event_id)
-	    new_db_event["start_dt"] = datetime.strptime(new_db_event["start_time"], "%Y-%m-%dT%H:%M:%S")
-            new_db_event["end_dt"] = datetime.strptime(new_db_event["end_time"], "%Y-%m-%dT%H:%M:%S")
-	    db_events.append(new_db_event)
+
+        db_events = []
+        db_event_ids = []
+        for db_event in FacebookEvent.objects.filter(facebook_user_id=fbuser["id"]):
+            db_event_ids.append(db_event.facebook_event_id)
+            new_db_event = fb(request).get_object(db_event.facebook_event_id)
+            new_db_event = start_end(new_db_event)
+            db_events.append(new_db_event)
 
         for event in events["data"]:
-            event["start_dt"] = datetime.strptime(event["start_time"], "%Y-%m-%dT%H:%M:%S")
-            event["end_dt"] = datetime.strptime(event["end_time"], "%Y-%m-%dT%H:%M:%S")
-            
+            event = start_end(event)
+
             if event["rsvp_status"] == u'attending' and event["start_dt"] > now:
-                new_event = request.facebook.graph.get_object(event["id"])
-                new_event["start_dt"] = datetime.strptime(event["start_time"], "%Y-%m-%dT%H:%M:%S")
-                new_event["end_dt"] = datetime.strptime(event["end_time"], "%Y-%m-%dT%H:%M:%S")
-                
+                new_event = fb(request).get_object(event["id"])
+                new_event = start_end(new_event)
+    
                 if new_event["owner"]["id"] == fbuser["id"] and new_event["privacy"] == u'OPEN' and not new_event["id"] in db_event_ids: 
                     real_events.append(new_event)
-        
-        return render_to_response('events.html', 
-			{'me':fbuser, 'events':real_events, 'db_events':db_events, 'friend_events':friend_events}, context_instance=c)
+
+        return render('events.html', {'me':fbuser, 'events':real_events, 'db_events':db_events}, request)
     else:
-        return render_to_response('index.html', {}, context_instance=c)
+        return render('index.html', {}, request)
 
 def logout_view(request):
     logout(request)
@@ -73,63 +99,44 @@ def mobile(request, event_id):
 
 def event(request,event_id):
     c = RequestContext(request)
-    matches = FacebookEvent.objects.filter(facebook_event_id=event_id)
-    success = False
-    if len(matches)==0:
-        if request.user.is_authenticated and request.user.is_active:
-            fbuser = request.facebook.graph.get_object("me")
-            fbevent = request.facebook.graph.get_object(event_id)
+    try:
+        event_obj = FacebookEvent.objects.get(facebook_event_id=event_id)
+    except FacebookEvent.DoesNotExist:
+        if fb(request) and logged_in(request):
+            fbuser = fb(request).get_object("me")
+            fbevent = fb(request).get_object(event_id)
             if fbevent["owner"]["id"]==fbuser["id"]: #create a QR code
                 event_obj = FacebookEvent(facebook_event_id=event_id,facebook_user_id=fbuser["id"])
                 event_obj.save()
-		success = True
-    else:
-        success = True
     
-    if success == True:
-        event = request.facebook.graph.get_object(event_id)
-        event["start_dt"] = datetime.strptime(event["start_time"], "%Y-%m-%dT%H:%M:%S")
-        event["end_dt"] = datetime.strptime(event["end_time"], "%Y-%m-%dT%H:%M:%S")
-	return render_to_response('event.html', {'event_id':event_id,'event_obj':event}, context_instance=c)
+    if event_obj:
+        event = fb(request).get_object(event_id)
+        event = start_end(event)
+        return render('event.html', {'event_id':event_id,'event_obj':event}, request)
     else:
-        return render_to_response('no_event.html', context_instance=c)
+        return render('no_event.html', {}, request)
 
 def email(request, event_id):
     if request.method == "POST":
-        api = facebook.GraphAPI()
-        event = api.get_object(event_id)
+        try:
+            addr = request.POST["email"].strip()
+            validate_email(addr)
+            
+            api = facebook.GraphAPI()
+            event = api.get_object(event_id)
+
+            content = ical.format("".join("".join(event["start_time"].split("-")).split(":")), 
+                                  "".join("".join(event["end_time"].split("-")).split(":")),
+                                  event["name"], event["location"], event["description"],
+                                  event["id"])
         
-        addr = request.POST["email"]
-    
-        content = """BEGIN:VCALENDAR
-        CALSCALE:GREGORIAN
-        X-WR-TIMEZONE;VALUE=TEXT:US/Eastern
-        METHOD:PUBLISH
-        PRODID:-//Apple Computer\, Inc//iCal 1.0//EN
-        X-WR-CALNAME;VALUE=TEXT:Example
-        VERSION:2.0
-        BEGIN:VEVENT
-        SEQUENCE:5
-        DTSTART;TZID=US/Eastern:{0}
-        DTSTAMP:{0}Z
-        SUMMARY:{1}
-        UID:{2}
-        DTEND;TZID=US/Eastern: {3}
-        BEGIN:VALARM
-        TRIGGER;VALUE=DURATION:-P1D
-        ACTION:DISPLAY
-        DESCRIPTION:Event reminder
-        END:VALARM
-        END:VEVENT
-        END:VCALENDAR""".format(event["start_time"].strip("-"), event["name"], str(uuid.uuid1()), event["end_time"].strip("-"))
+            email = mail.EmailMessage('Event from QREvents!', 'Hey there. Attached is the iCal event for {0}'.format(event["name"]),
+                                to=[addr])
+            email.attach("fbevent.ics", content, "text/calendar")
+            email.send()
         
-        connection = mail.get_connection()
-        
-        email = mail.EmailMessage('Event from QREvents!', 'Hey there. Attached is the iCal event for {0}'.format(event["name"]),
-                            to=[addr])
-        email.attach("fbevent.ics", content, "text/calendar")
-        email.send()
-        
-        return redirect(home)
+            return redirect('/m/'+event_id+"?success")
+        except forms.ValidationError:
+            return redirect('/m/'+event_id+"/?email="+quote(addr))
     
     
